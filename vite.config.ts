1211529bded7136e664585796da4dfc7039a422b
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-08-13 11:24:56 +0100

    Add support to convert vite env variables to be accessible in process.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), nodePolyfills()],
+    define: {
+      "process.env.REACT_APP_CONIQL_SOCKET": JSON.stringify(env.REACT_APP_CONIQL_SOCKET),
+      "process.env.REACT_APP_CONIQL_SSL": JSON.stringify(env.REACT_APP_CONIQL_SSL)
+    }
+  }
+
 })
