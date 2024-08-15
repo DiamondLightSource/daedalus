4f5fa71a3c259854c99bd05ea939c7f0b40fee48
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-08-15 15:45:00 +0100

    Add file validation in load click

--- src/components/FileNavigationBar.tsx
+++ src/components/FileNavigationBar.tsx
@@ -6,6 +6,7 @@ import { useContext, useState } from 'react';
 import { ADD_FILE, LOAD_NEXT_FILE } from '../store';
 import { EmbeddedDisplay, RelativePosition } from '@dls-controls/cs-web-lib';
 import { FileStateContext } from '../App';
+import { isValidHttpUrl } from '../utils/helper';
 
 const ALERT_MESSAGES: { [key: string]: string } = {
     success: "File was loaded successfully.",
@@ -17,50 +18,87 @@ export default function FileNavigationBar() {
     const { state, dispatch } = useContext(FileStateContext);
     const [filePath, setFilePath] = useState("");
     const [alert, setAlert] = useState<any>(undefined);
+    const [validFile, setValidFile] = useState<boolean | undefined>(true);
+    const [helperText, setHelperText] = useState<string>("");
 
     function handleTextChange(e: any) {
         setFilePath(e.target.value);
     }
 
-    function handleLoadButtonClick(e: any) {
-        // Validate - check if .bob, .opi or .json
-        // When click button, we create json
-        // If no file, give warning
+    /**
+     * Validate the file passed to load
+     * @param e 
+     */
+    async function handleLoadButtonClick(e: any) {
+        // Check if file exists
+        // If passes all checks, we load
+        // If fails, we change colour and set a popover explaining issue
+        if (filePath === "") setValidFile(true);
         const extension = filePath.split(".").pop();
-        if (!filePath) {
-            setAlert("warning");
-            // Change the last attempted load back to a failure
-        } else if (extension !== "bob" && extension !== "opi" && extension !== "json") {
-            // Change the last attempted load back to a failure
-            setAlert("error");
-        } else {
-            setAlert("success");
-            dispatch({ type: LOAD_NEXT_FILE, payload: { file: { path: filePath, macros: {} } } });
+        const validUrl = isValidHttpUrl(filePath)
+        const validExtension = (extension !== "bob" && extension !== "opi" && extension !== "json") ? false : true;
+        let valid = false;
+        try {
+            const fileExists = await fetch(filePath, { method: "HEAD" }).then((res) => { return res.ok });
+            if (fileExists) {
+                if (validUrl) {
+                    if (validExtension) {
+                        valid = true;
+                        setHelperText("");
+                        dispatch({ type: LOAD_NEXT_FILE, payload: { file: { path: filePath, macros: {} } } });
+                    } else {
+                        setHelperText("Invalid file extension. Use .bob, .opi or .json")
+                        valid = false;
+                    }
+                } else {
+                    setHelperText("Invalid URL protocol")
+                    valid = false;
+                }
+            } else {
+                setHelperText("Invalid file could not be fetched from URL")
+                valid = false;
+            }
+        } catch {
+            valid = false;
+            setHelperText("Invalid file could not be fetched from URL")
         }
-        setTimeout(() => {
-            setAlert(undefined);
-        }, 5000);
+
+        setValidFile(valid);
     }
 
     function handleSubmitButtonClick(e: any) {
         // Fetch the last loaded file info, and submit it
         // First fetch last loaded file
         if (state.nextFile.path) {
-            const display = (
-                <EmbeddedDisplay
-                    position={new RelativePosition()}
-                    file={
-                        {
-                            path: state.nextFile.path,
-                            macros: { ...state.nextFile.macros },
-                            defaultProtocol: "ca"
+            const extension = state.nextFile.path.split(".").pop();
+            if (extension !== "bob" && extension !== "opi" && extension !== "json") {
+                // Change the last attempted load back to a failure
+                setAlert("error");
+            } else {
+                const display = (
+                    <EmbeddedDisplay
+                        height={800}
+                        position={new RelativePosition()}
+                        scroll={true}
+                        resize={0}
+                        file={
+                            {
+                                path: state.nextFile.path,
+                                macros: { ...state.nextFile.macros },
+                                defaultProtocol: "ca"
+                            }
                         }
-                    }
-                    scroll={true}
-                />)
-            dispatch({ type: ADD_FILE, payload: { name: state.nextFile.path, display: display } });
-            // Update macros
+                    />
+                )
+                dispatch({ type: ADD_FILE, payload: { name: state.nextFile.path, display: display } });
+                setAlert("success");
+            }
+        } else {
+            setAlert("warning");
         }
+        setTimeout(() => {
+            setAlert(undefined);
+        }, 5000);
     }
 
     return (
@@ -68,10 +106,10 @@ export default function FileNavigationBar() {
             {alert ? <Alert severity={alert} sx={{ position: "absolute", top: -80, zIndex: 1300, width: "100%", height: 65, textAlign: "center", justifyContent: "center", display: "flex" }}><AlertTitle>{alert.toUpperCase()}</AlertTitle>{ALERT_MESSAGES[alert]}</Alert> : <></>}
             <Stack spacing={2} direction="row" sx={{ top: 20, left: 0, zIndex: 1299 }}>
                 <Typography sx={{ display: "flex", alignItems: "center", height: 50 }}>File to load:</Typography>
-                <TextField id="outlined-basic" label="URL" variant="outlined" onChange={handleTextChange} />
-                <Button sx={{ height: 40 }} variant="contained" onClick={handleLoadButtonClick}>Load</Button>
+                <TextField id="outlined-basic" value={filePath} label="URL" variant="outlined" onChange={handleTextChange} sx={{ height: 50 }} error={!validFile} helperText={helperText} />
+                <Button sx={{ height: 50 }} variant="contained" onClick={handleLoadButtonClick}>Load</Button>
                 <MacrosModal></MacrosModal>
-                <Button sx={{ height: 40 }} variant="contained" onClick={handleSubmitButtonClick}>Submit</Button>
+                <Button sx={{ height: 50 }} variant="contained" onClick={handleSubmitButtonClick}>Submit</Button>
             </Stack>
         </Container>
     );
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -40,3 +40,15 @@ export function useWindowHeight() {
 
     return windowHeight;
 }
+
+export function isValidHttpUrl(text: string) {
+    let url;
+
+    try {
+        url = new URL(text);
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
+}
