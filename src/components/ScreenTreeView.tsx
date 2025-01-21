7bad04b741e17f565772cae31a02c1e43daa0c33
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2025-01-21 11:59:46 +0000

    Link beamline dropdown to displayed treeview in menu bar

--- src/components/ScreenTreeView.tsx
+++ src/components/ScreenTreeView.tsx
@@ -2,6 +2,7 @@ import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 import { useContext } from 'react';
 import BeamlineTreeStateContext from '../routes/MainPage';
 import { CHANGE_SCREEN } from '../store';
+import { TreeViewBaseItem } from '@mui/x-tree-view';
 
 
 export default function ScreenTreeView() {
@@ -11,9 +12,13 @@ export default function ScreenTreeView() {
         dispatch({ type: CHANGE_SCREEN, payload: { screenId: itemId } });
     };
 
+    let currentScreenTree: TreeViewBaseItem[] = [];
+    const currentBeamline = state.beamlines.filter(item => item.beamline === state.currentBeamline)
+    if (currentBeamline.length > 0) currentScreenTree = currentBeamline[0].screenTree;
+
     return (
         <>
-            {state.menuBarOpen ? <RichTreeView items={state.beamlines[0].screenTree} onItemClick={(event, itemId) => handleClick(itemId)} /> : <></>}
+            {state.menuBarOpen ? <RichTreeView items={currentScreenTree} onItemClick={(event, itemId) => handleClick(itemId)} /> : <></>}
         </>
     );
 }
