3eae27a3f8dd71bf9703af43c7d32acc8be4c8b1
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2025-02-04 11:36:35 +0000

    Move breadcrumns into App bar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -5,9 +5,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import { useContext } from 'react';
 import BeamlineTreeStateContext from '../routes/MainPage';
 import { APP_BAR_HEIGHT, DRAWER_WIDTH } from '../utils/helper';
+import { Breadcrumbs, Link } from '@mui/material';
+import { executeAction, FileContext } from '@diamondlightsource/cs-web-lib';
 
 interface AppBarProps extends MuiAppBarProps {
     open?: boolean;
@@ -44,20 +47,49 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 export default function DLSAppBar() {
-    const { state, dispatch } = useContext(BeamlineTreeStateContext);
+    const { state } = useContext(BeamlineTreeStateContext);
+    const fileContext = useContext(FileContext)
 
     const handleOpenSettings = () => {
         console.log("TO DO - create settings modal")
     };
+
+    function handleClick(event: any) {
+        if (event.target.pathname) {
+            event.preventDefault();
+            const screenId = decodeURI(event.target.pathname).split("/").at(-1) as string;
+            const newScreen = state.beamlines[state.currentBeamline].filePathIds[screenId];
+            executeAction({
+                type: 'OPEN_PAGE',
+                dynamicInfo: {
+                    name: newScreen,
+                    location: "main",
+                    description: undefined,
+                    file: {
+                        path: newScreen,
+                        macros: {},
+                        defaultProtocol: "ca"
+                    }
+                }
+            }, fileContext, undefined, {}, event.target.pathname)
+        }
+    }
+
+    const breadcrumbs = createBreadcrumbs(state.currentScreenId, state.currentBeamline)
 
     return (
         <>
             <CssBaseline />
             <AppBar position="absolute" open={state.menuBarOpen} sx={{ height: APP_BAR_HEIGHT }}>
                 <Toolbar>
-                    <Typography variant="h1" component="div" sx={{ flexGrow: 1, textAlign: "center" }}>
-                        Daedalus Demo
-                    </Typography>
+                    <Breadcrumbs
+                        onClick={handleClick}
+                        separator={<NavigateNextIcon fontSize="small" />}
+                        aria-label="breadcrumb"
+                        sx={{ marginBottom: "10px", p: 2, paddingBottom: 0, textAlign: "left", color: "white" }}
+                    >
+                        {breadcrumbs}
+                    </Breadcrumbs>
                     <IconButton
                         color="inherit"
                         aria-label="open drawer"
@@ -70,4 +102,33 @@ export default function DLSAppBar() {
             </AppBar>
         </>
     );
+}
+
+
+/**
+ * Navigates the screen Treeview to determine the correct
+ * breadcrumb trail
+ */
+function createBreadcrumbs(screenId: string, beamline: string) {
+
+    const breadcrumbs: any[] = [];
+    if (beamline === "") return [];
+
+    const breadcrumbLabels = screenId.split("-")
+    let linkUrl = `/${beamline}/`
+    breadcrumbLabels.forEach((label, idx) => {
+        if (idx !== 0) linkUrl += "-"
+        linkUrl += label;
+        breadcrumbs.push(
+            (idx === breadcrumbLabels.length - 1) ?
+                <Typography key="3" sx={{ color: 'white' }}>
+                    {label}
+                </Typography>
+                :
+                <Link underline="hover" key={idx} color="white" href={linkUrl}>
+                    {label}
+                </Link>
+        )
+    })
+    return breadcrumbs;
 }
--- src/components/ScreenDisplay.tsx
+++ src/components/ScreenDisplay.tsx
@@ -1,10 +1,8 @@
 import { useContext, useEffect } from 'react';
 import BeamlineTreeStateContext from '../routes/MainPage';
-import { Box, Divider, Link, Paper as MuiPaper, PaperProps as MuiPaperProps, styled, Typography } from '@mui/material';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { Box, Paper as MuiPaper, PaperProps as MuiPaperProps, styled } from '@mui/material';
 import { useHistory, useLocation } from 'react-router-dom';
-import { DynamicPageWidget, executeAction, FileContext, RelativePosition } from '@diamondlightsource/cs-web-lib';
+import { DynamicPageWidget, FileContext, RelativePosition } from '@diamondlightsource/cs-web-lib';
 import { useWindowWidth, useWindowHeight, DRAWER_WIDTH, APP_BAR_HEIGHT } from '../utils/helper';
 
 
@@ -39,27 +37,6 @@ export default function ScreenDisplay() {
     const history = useHistory();
     const location = useLocation();
 
-    const breadcrumbs = createBreadcrumbs(state.currentScreenId, state.currentBeamline)
-
-    function handleClick(event: any) {
-        event.preventDefault();
-        const screenId = decodeURI(event.target.pathname).split("/").at(-1) as string;
-        const newScreen = state.beamlines[state.currentBeamline].filePathIds[screenId];
-        executeAction({
-            type: 'OPEN_PAGE',
-            dynamicInfo: {
-                name: newScreen,
-                location: "main",
-                description: undefined,
-                file: {
-                    path: newScreen,
-                    macros: {},
-                    defaultProtocol: "ca"
-                }
-            }
-        }, fileContext, undefined, {}, event.target.pathname)
-    }
-
     useEffect(() => {
         // This catches file changes done inside the file by actionbuttons
         // and updates the URL to match the fileroute
@@ -77,62 +54,17 @@ export default function ScreenDisplay() {
     return (
         <Paper component="main" open={state.menuBarOpen} >
             <Box>
-                {breadcrumbs.length === 0 ?
-                    <Typography sx={{ marginBottom: 2 }}>
-                        Homepage! You have no file loaded
-                    </Typography>
-                    :
-                    <>
-                        <Breadcrumbs
-                            onClick={handleClick}
-                            separator={<NavigateNextIcon fontSize="small" />}
-                            aria-label="breadcrumb"
-                            sx={{ marginBottom: "10px", p: 2, paddingBottom: 0 }}
-                        >
-                            {breadcrumbs}
-                        </Breadcrumbs>
-                        <Divider variant="fullWidth" sx={{ width: "100%" }} />
-                        <Box>
-                            {state.currentBeamline && state.currentScreenId ?
-                                <DynamicPageWidget
-                                    location={"main"}
-                                    position={new RelativePosition()}
-                                    scroll={false}
-                                    showCloseButton={false}
-                                />
-                                : <></>}
-                        </Box>
-                    </>
-                }
+                <Box>
+                    {state.currentBeamline && state.currentScreenId ?
+                        <DynamicPageWidget
+                            location={"main"}
+                            position={new RelativePosition()}
+                            scroll={false}
+                            showCloseButton={false}
+                        />
+                        : <></>}
+                </Box>
             </Box>
         </Paper >
     );
-}
-
-/**
- * Navigates the screen Treeview to determine the correct
- * breadcrumb trail
- */
-function createBreadcrumbs(screenId: string, beamline: string) {
-
-    const breadcrumbs: any[] = [];
-    if (beamline === "") return [];
-
-    const breadcrumbLabels = screenId.split("-")
-    let linkUrl = `/${beamline}/`
-    breadcrumbLabels.forEach((label, idx) => {
-        if (idx !== 0) linkUrl += "-"
-        linkUrl += label;
-        breadcrumbs.push(
-            (idx === breadcrumbLabels.length - 1) ?
-                <Typography key="3" sx={{ color: 'text.primary' }}>
-                    {label}
-                </Typography>
-                :
-                <Link underline="hover" key={idx} color="inherit" href={linkUrl}>
-                    {label}
-                </Link>
-        )
-    })
-    return breadcrumbs;
 }
