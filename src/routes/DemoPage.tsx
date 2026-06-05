1a3918587a24b186c857fe644d47fcc97aef0d1e
Greg Harris <gregory.harris@diamond.ac.uk>
2026-06-05 17:38:56 +0100

    Adding some screens to demo auto layout

--- src/routes/DemoPage.tsx
+++ src/routes/DemoPage.tsx
@@ -23,7 +23,7 @@ export function DemoPage() {
 
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
+      <Box id="DemoPage-Box" sx={{ display: "flex", height: "100dvh", width: "100%" }}>
         <DLSAppBar fullScreen={true} open={false}>
           <Typography
             variant="h3"
@@ -35,6 +35,7 @@ export function DemoPage() {
         </DLSAppBar>
         <FileStateContext.Provider value={{ state, dispatch }}>
           <Stack
+            id="DemoPage-stack"
             sx={{
               alignItems: "center",
               position: "absolute",
