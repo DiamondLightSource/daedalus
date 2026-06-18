f28a11f8e64acdf5298f41299bcacd3e926d1152
Greg Harris <gregory.harris@diamond.ac.uk>
2026-06-18 14:23:19 +0100

    Lint fix

--- src/components/ScreenDisplay.tsx
+++ src/components/ScreenDisplay.tsx
@@ -88,7 +88,7 @@ export default function ScreenDisplay() {
           scroll={false}
           showCloseButton={false}
           mjpgEndpoint={beamlineState?.mjpgEndpoint}
-          widgetIdsCallback={(uuid) => {
+          widgetIdsCallback={uuid => {
             // The uuid allows the json representation of the display instance to be selected from the redux store
             displayUuidRef.current = uuid;
           }}
